20bc1c9a45edb7dc3da77f87d87bd9146be385f7
Adam Díaz <79486323+Adam19111987@users.noreply.github.com>
2024-09-06 14:01:31 -0400

    Actualizar script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,8 @@
-
 function cambiarColor(elemento, color = "green") {
-    
-    elemento.classList.remove('green', 'yellow');
-
-    if (color === "yellow") {
-        elemento.classList.add('yellow');
-    } else {
-        elemento.classList.add('green');
-    }
+    elemento.style.background = color;
 }
-
 
 const ele = document.querySelector("#ele1");
 ele.addEventListener("click", function() {
-   
-    if (ele.classList.contains('green')) {
-        cambiarColor(ele, 'yellow');
-    } else {
-        cambiarColor(ele, 'green');
-    }
+    cambiarColor(ele, 'yellow');
 });
